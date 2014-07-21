d773a1723193759f05a7ae28dcd1732f39044f06
ivadenis <ivadenis@gmail.com>
2014-07-21 10:22:21 +0200

    step2.rb added

---
+++ lab1/step2.rb
@@ -0,0 +1,20 @@
+=begin
+Write an implementation of a function map that applies a block to all elements of an array and returns an array of the results. For example,
+```map([1,2,3]) { |x| x * x }```
+should return
+
+```[1,4,9]```
+Hint: Adapt the filter function
+
+What is the code of your function?
+=end
+
+def map(arr)
+  r = []
+  for i in arr
+    r.push( yield(i) )
+  end
+  return r
+end
+map([1,2,3]) { |x| x * x }
+map([4,5,6]) { |y| y + 1 }
